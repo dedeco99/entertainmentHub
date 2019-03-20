b20183013500a99cb08f555ec1e73d76a48584a2
dedeco99 <dedeco99@gmail.com>
2019-03-20 20:33:22 +0000

    Fixed deployment issue with heroku

--- server/database.js
+++ server/database.js
@@ -1,9 +1,19 @@
 const admin = require("firebase-admin");
-const firebaseSecret = require("./firebaseSecret");
 
 exports.initialize = () => {
 	admin.initializeApp({
-		credential: admin.credential.cert(firebaseSecret)
+		credential: admin.credential.cert({
+			type: process.env.firebaseType,
+			project_id: process.env.firebaseSecretProjectId,
+			private_key_id: process.env.firebaseSecretPrivateKeyId,
+			private_key: process.env.firebaseSecretPrivateKey.replace(/\\n/g, '\n'),
+			client_email: process.env.firebaseSecretClientEmail,
+			client_id: process.env.firebaseSecretClientId,
+			auth_uri: process.env.firebaseSecretAuthUri,
+			token_ri: process.env.firebaseSecretTokenUri,
+			auth_provider_x509_cert_url: process.env.firebaseSecretAuthProvider,
+			client_x509_cert_url: process.env.firebaseSecretClientCert
+		})
 	});
 
 	exports.firestore = admin.firestore();
