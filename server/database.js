966c56ef8cd0704630c83e666790734204946986
André Coelho <dedeco99@gmail.com>
2019-08-19 23:55:19 +0100

    Migrated to mongoDb database

--- server/database.js
+++ server/database.js
@@ -1,22 +1,19 @@
-const admin = require("firebase-admin");
+const mongoose = require("mongoose");
 
-exports.initialize = () => {
-	admin.initializeApp({
-		credential: admin.credential.cert({
-			type: process.env.firebaseType,
-			project_id: process.env.firebaseSecretProjectId,
-			private_key_id: process.env.firebaseSecretPrivateKeyId,
-			private_key: process.env.firebaseSecretPrivateKey.replace(/\\n/g, '\n'),
-			client_email: process.env.firebaseSecretClientEmail,
-			client_id: process.env.firebaseSecretClientId,
-			auth_uri: process.env.firebaseSecretAuthUri,
-			token_ri: process.env.firebaseSecretTokenUri,
-			auth_provider_x509_cert_url: process.env.firebaseSecretAuthProvider,
-			client_x509_cert_url: process.env.firebaseSecretClientCert
-		})
-	});
+const secrets = require("./secrets");
 
-	exports.firestore = admin.firestore();
-}
+mongoose.set("useFindAndModify", false);
+mongoose.connect(secrets.databaseConnectionString, { useNewUrlParser: true });
 
-exports.firestore = null;
+const Auth = require("./models/auth");
+
+/* Channel */
+
+const getAuth = async (query) => {
+	console.log(query);
+	return await Auth.findOne(query);
+};
+
+module.exports = {
+	getAuth
+};
