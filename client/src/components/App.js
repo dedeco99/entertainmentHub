1e071404fd30c17d0d572ec91642f2156de6aea7
André Coelho <dedeco99@gmail.com>
2019-08-20 20:14:05 +0100

    Fixed some stuff to not crash

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,34 +6,36 @@ import Header from "./header/Header";
 import Index from "./Index";
 import Register from "./auth/Register";
 import Login from "./auth/Login";
-import Reddit from "./reddit/Reddit";
+/*import Reddit from "./reddit/Reddit";
 import Youtube from "./youtube/Youtube";
 import Twitch from "./twitch/Twitch";
-import TVSeries from "./tvseries/TVSeries";
+import TVSeries from "./tvseries/TVSeries";*/
 import Settings from "./settings/Settings";
 
 import "../css/App.css";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <div className="main container-fluid">
-          <Switch>
-            <Route exact path="/" component={ Index } />
-            <Route exact path="/register" component={ Register } />
-            <Route exact path="/login" component={ Login } />
-            <PrivateRoute exact path="/reddit/:sub?/:category?" component={ Reddit } />
-            <PrivateRoute exact path="/youtube" component={ Youtube } />
-            <PrivateRoute exact path="/twitch" component={ Twitch } />
-            <PrivateRoute exact path="/tvseries" component={ TVSeries } />
-            <PrivateRoute exact path="/settings" component={ Settings } />
-          </Switch>
-        </div>
-      </div>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<Header />
+				<div className="main container-fluid">
+					<Switch>
+						<Route exact path="/" component={Index} />
+						<Route exact path="/register" component={Register} />
+						<Route exact path="/login" component={Login} />
+						{/*
+							<PrivateRoute exact path="/reddit/:sub?/:category?" component={ Reddit } />
+							<PrivateRoute exact path="/youtube" component={ Youtube } />
+							<PrivateRoute exact path="/twitch" component={ Twitch } />
+							<PrivateRoute exact path="/tvseries" component={ TVSeries } />
+						*/}
+						<PrivateRoute exact path="/settings" component={Settings} />
+					</Switch>
+				</div>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
