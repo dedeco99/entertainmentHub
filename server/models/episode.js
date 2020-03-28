17190ccb56accc76b2d885898972c37ea4903a9c
dedeco99 <dedeco99@gmail.com>
2020-03-28 12:23:08 +0000

    Changed season and episode to Number in episode model

--- server/models/episode.js
+++ server/models/episode.js
@@ -6,8 +6,8 @@ const EpisodeSchema = new Schema({
 	seriesId: { type: String, default: null },
 	title: { type: String, default: "" },
 	image: { type: String, default: "" },
-	season: { type: String, default: null },
-	number: { type: String, default: null },
+	season: { type: Number, default: null },
+	number: { type: Number, default: null },
 	date: { type: Date, default: null },
 }, { timestamps: { createdAt: "_created", updatedAt: "_modified" } });
 
