f92cf4e1936b13a851a5ab63e494cd540d2db2fd
Pedro <180221091@estudantes.ips.pt>
2020-05-03 16:07:20 +0100

    Made twitch widget prettier

--- client/src/components/widgets/Twitch.js
+++ client/src/components/widgets/Twitch.js
@@ -6,11 +6,12 @@ import Zoom from "@material-ui/core/Zoom";
 import Card from "@material-ui/core/Card";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import Box from "@material-ui/core/Box";
 
 import { getStreams } from "../../api/twitch";
 
-import { reddit as styles } from "../../styles/Widgets";
+import { twitch as styles } from "../../styles/Widgets";
+import { Typography } from "@material-ui/core";
 
 class Twitch extends Component {
 	constructor() {
@@ -29,20 +30,26 @@ class Twitch extends Component {
 	async getStreams() {
 		const response = await getStreams();
 
+		console.log(response);
+
 		this.setState({ streams: response.data, open: true });
 	}
 
 	renderStreamsList() {
+		const { classes } = this.props;
 		const { streams } = this.state;
 
 		const streamsList = streams.map(stream => (
 			<ListItem key={stream.id} button divider>
-				<ListItemText
-					primary={stream.title}
-					title={stream.title}
-					secondary={stream.user}
-					primaryTypographyProps={{ noWrap: true }}
-				/>
+				<Box flex="1" flexGrow={1} className={classes.imageWrapper}>
+					<img alt={`${stream.user}-preview`} src={stream.thumbnail} width="100%" />
+					<Typography variant="caption" className={classes.viewers}> {stream.viewers} </Typography>
+				</Box>
+				<Box p={1} flex="1" flexGrow={2} minWidth="0%">
+					<Typography variant="body1" noWrap> {stream.user} </Typography>
+					<Typography variant="body2" noWrap> {stream.title} </Typography>
+					<Typography variant="subtitle2" noWrap> {stream.game} </Typography>
+				</Box>
 			</ListItem >
 		));
 
@@ -55,10 +62,7 @@ class Twitch extends Component {
 
 		return (
 			<Zoom in={open}>
-				<Card
-					variant="outlined"
-					className={classes.root}
-				>
+				<Card variant="outlined" className={classes.root}>
 					{this.renderStreamsList()}
 				</Card>
 			</Zoom>
