a7238ed2655d90ae58a96890ddc6ac9048c9c8c6
JoaoRosete <rosete_jp@hotmail.com>
2020-07-27 13:58:23 +0100

    Feed FrontEnd

--- client/src/components/youtube/Feed.js
+++ client/src/components/youtube/Feed.js
@@ -5,6 +5,9 @@ import { makeStyles } from "@material-ui/styles";
 import Zoom from "@material-ui/core/Zoom";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
+import ListItem from "@material-ui/core/ListItem";
+import IconButton from "@material-ui/core/IconButton";
 
 import Widget from "../widgets/Widget";
 import ChannelGroupDetail from "./ChannelGroupDetail";
@@ -16,6 +19,7 @@ import { deleteChannelGroup } from "../../api/channelGroups";
 
 import { widget as widgetStyles } from "../../styles/Widgets";
 import { feed as feedStyles } from "../../styles/Youtube";
+import { formatDate } from "../../utils/utils";
 
 const useStyles = makeStyles({ ...widgetStyles, ...feedStyles });
 
@@ -55,6 +59,47 @@ function Feed({ channelGroup }) {
 		setOpenModal(false);
 	}
 
+	function renderVideos() {
+		console.log(videos);
+		return videos.map(video => (
+			<ListItem key={video.videoId} divider>
+				<Box position="relative" flexShrink="0" width="100px" mr={2}>
+					<img src={video.thumbnail} width="100%" alt="Video thumbnail" />
+					<Box position="absolute" bottom="0" right="0" px={0.5} style={{ backgroundColor: "#212121DD" }}>
+						<Typography variant="caption"> {"Duration"} </Typography>
+					</Box>
+				</Box>
+				<Box display="flex" flexDirection="column" flex="auto" minWidth={0}>
+					<Typography variant="body1" title={video.videoTitle} noWrap>
+						<Link href={`https://www.youtube.com/watch?v=${video.videoId}`} target="_blank" rel="noreferrer" color="inherit">
+							{video.videoTitle}
+						</Link>
+					</Typography>
+					<Typography variant="body2" title={video.displayName}>
+						<Link href={`https://www.youtube.com/channel/${video.channelId}`} target="_blank" rel="noreferrer" color="inherit">
+							{video.displayName}
+						</Link>
+					</Typography>
+					<Typography variant="caption"> {formatDate(video.published, "DD-MM-YYYY HH:mm")} </Typography>
+					<Box display="flex" flexDirection="row" flex="1 1 auto" minWidth={0}>
+						<Typography variant="caption" style={{ paddingRight: "10px" }}>
+							<IconButton size="small" style={{ marginBottom: "3px" }}>
+								<i className="icofont-thumbs-up" />
+							</IconButton>
+							{"100"}
+						</Typography>
+						<Typography variant="caption">
+							<IconButton size="small" style={{ marginTop: "1px" }}>
+								<i className="icofont-thumbs-down" />
+							</IconButton>
+							{"100"}
+						</Typography>
+					</Box>
+				</Box>
+			</ListItem>
+		));
+	}
+
 	function renderFeed() {
 		return (
 			<Zoom in={open}>
@@ -67,19 +112,9 @@ function Feed({ channelGroup }) {
 						flexWrap="wrap"
 						justifyContent="center"
 						height="100%"
-						style={{ overflow: "auto", padding: 10 }}
+						style={{ overflow: "auto", width: "inherit" }}
 					>
-						{videos.map(video => (
-							<div key={video.videoId}>
-								{video.videoTitle}
-								<br />
-								{video.displayName}
-								<br />
-								{video.published}
-								<br />
-								<br />
-							</div>
-						))}
+						{ renderVideos() }
 					</Box>
 				</Box>
 			</Zoom>
