875fc70fa0651b60eaee7ac5f3774cf06af7d607
Pedro <180221091@estudantes.ips.pt>
2020-07-29 22:47:51 +0100

    Changed settings

--- client/src/components/settings/Settings.js
+++ client/src/components/settings/Settings.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { withStyles } from "@material-ui/styles";
 import PropTypes from "prop-types";
 
@@ -12,94 +12,98 @@ import FormControl from "@material-ui/core/FormControl";
 import Checkbox from "@material-ui/core/Checkbox";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import Divider from "@material-ui/core/Divider";
 
 import { UserContext } from "../../contexts/UserContext";
 
 import { getApps, deleteApp } from "../../api/apps";
 import { editUser } from "../../api/users";
 
-import reddit from "../../img/reddit.png";
-import twitch from "../../img/twitch.png";
-import youtube from "../../img/youtube.png";
-import tv from "../../img/tv.png";
-
 import { settings as styles } from "../../styles/Header";
 
-const REDIRECT = "https://entertainmenthub.ddns.net";
+function Settings({ classes }) {
+	const REDIRECT = "https://entertainmenthub.ddns.net";
 
-class Settings extends Component {
-	constructor() {
-		super();
-		this.state = {
-			apps: {
-				reddit: {
-					active: false,
-					key: "reddit",
-					displayName: "Reddit",
-					link: `https://www.reddit.com/api/v1/authorize
-						?client_id=VXMNKvXfKALA3A
-						&response_type=code
-						&state=some_state
-						&redirect_uri=${REDIRECT}/apps/reddit
-						&duration=permanent
-						&scope=read`,
-				},
-				twitch: {
-					active: false,
-					key: "twitch",
-					displayName: "Twitch",
-					link: `https://api.twitch.tv/kraken/oauth2/authorize
-						?client_id=9haxv452ih4k8ewiml53vqetrbm0z9q
-						&response_type=code
-						&redirect_uri=${REDIRECT}/apps/twitch
-						&scope=user_read`,
-				},
-				youtube: {
-					active: false,
-					key: "youtube",
-					displayName: "Youtube",
-					link: `https://accounts.google.com/o/oauth2/v2/auth
-						?redirect_uri=${REDIRECT}/apps/youtube
-						&prompt=consent
-						&access_type=offline
-						&response_type=code
-						&client_id=539994951120-kabifq9ct2lbk92m9ef4hddc5f57nksl.apps.googleusercontent.com
-						&scope=https://www.googleapis.com/auth/youtube`,
-				},
-				tv: {
-					active: false,
-					key: "tv",
-					displayName: "TV",
-					link: "/apps/tv",
-				},
-			},
+	const { user, dispatch } = useContext(UserContext);
 
-			selectedMenu: 0,
-			settings: {},
-		};
+	const [apps, setApps] = useState({
+		reddit: {
+			active: false,
+			key: "reddit",
+			displayName: "Reddit",
+			icon: "icofont-reddit",
+			link: `https://www.reddit.com/api/v1/authorize
+				?client_id=VXMNKvXfKALA3A
+				&response_type=code
+				&state=some_state
+				&redirect_uri=${REDIRECT}/apps/reddit
+				&duration=permanent
+				&scope=read`,
+			color: "#fd4500",
+		},
+		twitch: {
+			active: false,
+			key: "twitch",
+			displayName: "Twitch",
+			icon: "icofont-twitch",
+			link: `https://api.twitch.tv/kraken/oauth2/authorize
+				?client_id=9haxv452ih4k8ewiml53vqetrbm0z9q
+				&response_type=code
+				&redirect_uri=${REDIRECT}/apps/twitch
+				&scope=user_read`,
+			color: "#6441a5",
+		},
+		youtube: {
+			active: false,
+			key: "youtube",
+			displayName: "Youtube",
+			icon: "icofont-youtube-play",
+			link: `https://accounts.google.com/o/oauth2/v2/auth
+				?redirect_uri=${REDIRECT}/apps/youtube
+				&prompt=consent
+				&access_type=offline
+				&response_type=code
+				&client_id=539994951120-kabifq9ct2lbk92m9ef4hddc5f57nksl.apps.googleusercontent.com
+				&scope=https://www.googleapis.com/auth/youtube`,
+			color: "linear-gradient(0deg, rgba(226,43,40,1) 0%, rgba(191,31,19,1) 100%)",
+		},
+		tv: {
+			active: false,
+			key: "tv",
+			displayName: "TV",
+			icon: "icofont-monitor",
+			link: "/apps/tv",
+			color: "linear-gradient(0deg, rgba(1,97,234,1) 0%, rgba(0,187,250,1) 100%)",
+		},
+	});
+	const [selectedMenu, setSelectedMenu] = useState(0);
+	const [settings, setSettings] = useState({});
 
-		this.handleListClick = this.handleListClick.bind(this);
-		this.handleChangeScrollbar = this.handleChangeScrollbar.bind(this);
-		this.handleAnimations = this.handleAnimations.bind(this);
-		this.handleBorderColor = this.handleBorderColor.bind(this);
-		this.handleSubmitSettings = this.handleSubmitSettings.bind(this);
+	async function getAppsCall() {
+		const response = await getApps();
+
+		if (response.data.length) {
+			for (const userApp of response.data) {
+				apps[userApp.platform].id = userApp._id;
+				apps[userApp.platform].active = true;
+			}
+
+			setApps({ ...apps });
+		}
 	}
 
-	componentDidMount() {
-		this.getApps();
-		this.getSettings();
-	}
+	useEffect(() => {
+		async function fetchData() {
+			setSettings(user.settings || {});
+			await getAppsCall();
+		}
 
-	getSettings() {
-		const { user } = this.context;
+		fetchData();
+	}, []); // eslint-disable-line
 
-		this.setState({ settings: user.settings || {} });
-	}
-
-	async handleSubmitSettings() {
-		const { user, dispatch } = this.context;
-		const { settings } = this.state;
-
+	async function handleSubmitSettings() {
 		const response = await editUser({ settings });
 
 		if (response.data) {
@@ -109,99 +113,62 @@ class Settings extends Component {
 		}
 	}
 
-	async getApps() {
-		const { apps } = this.state;
-
-		const response = await getApps();
-
-		if (response.data.length) {
-			for (const userApp of response.data) {
-				apps[userApp.platform].id = userApp._id;
-				apps[userApp.platform].active = true;
-			}
-
-			this.setState({ apps });
-		}
+	async function handleDeleteApp(appId) {
+		await deleteApp(appId);
 	}
 
-	async handleDeleteApp(e) {
-		await deleteApp(e.target.id);
+	function handleListClick(index) {
+		setSelectedMenu(index);
 	}
 
-	handleListClick(index) {
-		this.setState({ selectedMenu: index });
+	function handleCheckboxChange(property) {
+		setSettings({ ...settings, [property]: !settings[property] });
 	}
 
-	handleChangeScrollbar() {
-		const { settings } = this.state;
-
-		settings.useCustomScrollbar = !settings.useCustomScrollbar;
-
-		this.setState({ settings });
-	}
-
-	handleAnimations() {
-		const { settings } = this.state;
-
-		settings.animations = !settings.animations;
-
-		this.setState({ settings });
-	}
-
-	handleBorderColor() {
-		const { settings } = this.state;
-
-		settings.borderColor = !settings.borderColor;
-
-		this.setState({ settings });
-	}
-
-
-	renderApp(app) {
-		const { classes } = this.props;
-		const images = {
-			reddit,
-			twitch,
-			youtube,
-			tv,
-		};
-
-		if (app.active) {
-			return (
-				<Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={app.key}>
-					<div className={classes.appsContainer}>
-						<img src={images[app.key]} width="100%" alt={app.displayName} />
-						<i className={`${classes.delete} material-icons`} id={app.id} onClick={this.handleDeleteApp}>
-							{"delete"}
-						</i>
-					</div>
-				</Grid>
-			);
-		}
-
+	function renderApp(app) {
 		return (
-			<Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={app.key}>
-				<a href={app.link} target="_self">
-					<img src={images[app.key]} width="100%" alt={app.displayName} />
-				</a>
-			</Grid>
+			<Box key={app.key}>
+				<Box p={2} mb={1} display="flex">
+					<Box mr={2}>
+						<Box p={2} borderRadius="4px" style={{ background: app.color }}>
+							<i className={`${app.icon} icofont-3x`} />
+						</Box>
+					</Box>
+					<Box flexGrow={1}>
+						<Box display="flex" alignItems="flex-start" mb={1}>
+							<Box flexGrow={1}>
+								<Typography> {app.displayName} </Typography>
+								{app.active ? (
+									<Typography variant="body2"> <i className="icofont-check-circled" /> {`Your ${app.displayName} is connected`} </Typography>
+								) : (
+									<Typography variant="body2"> <i className="icofont-close-circled" /> {`Your ${app.displayName} is not currently connected`} </Typography>
+								)}
+							</Box>
+							{app.active ? (
+								<Button color="primary" variant="contained" size="small" onClick={() => handleDeleteApp(app.id)}> {"Disconnect"} </Button>
+							) : (
+								<Button color="primary" variant="contained" size="small" href={app.link} target="_self"> {"Connect"} </Button>
+							)}
+						</Box>
+						<Typography variant="caption">
+							{"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus tortor nec ex sodales, vitae malesuada metus mollis. Nullam consectetur egestas fringilla. Nullam turpis sapien, sodales et ex nec, mollis vestibulum odio. Vestibulum sed dui orci."}
+						</Typography>
+					</Box>
+				</Box>
+				<Divider />
+			</Box>
 		);
 	}
 
-	renderApps() {
-		const { apps } = this.state;
-
+	function renderApps() {
 		return (
-			<Grid container spacing={2}>
-				{Object.keys(apps).map(app => this.renderApp(apps[app]))}
-			</Grid>
+			<Paper>
+				{Object.keys(apps).map(app => renderApp(apps[app]))}
+			</Paper>
 		);
 	}
 
-	renderSettings() {
-		const { settings } = this.state;
-		const { classes } = this.props;
-
+	function renderSettings() {
 		return (
 			<div className={classes.settingsContainer}>
 				<Typography variant="h4"> {"Change settings"} </Typography>
@@ -211,7 +178,7 @@ class Settings extends Component {
 							<Checkbox
 								checked={settings.useCustomScrollbar || false}
 								color="primary"
-								onChange={this.handleChangeScrollbar}
+								onChange={() => handleCheckboxChange("useCustomScrollbar")}
 							/>
 						}
 						label="Use custom scrollbar"
@@ -221,7 +188,7 @@ class Settings extends Component {
 							<Checkbox
 								checked={settings.animations || false}
 								color="primary"
-								onChange={this.handleAnimations}
+								onChange={() => handleCheckboxChange("animations")}
 							/>
 						}
 						label="Animations"
@@ -231,71 +198,62 @@ class Settings extends Component {
 							<Checkbox
 								checked={settings.borderColor || false}
 								color="primary"
-								onChange={this.handleBorderColor}
+								onChange={() => handleCheckboxChange("borderColor")}
 							/>
 						}
 						label="Border color on widgets"
 					/>
 				</FormControl>
-				<Button variant="contained" onClick={this.handleSubmitSettings}> {"Apply"} </Button>
+				<Button variant="contained" onClick={handleSubmitSettings}> {"Apply"} </Button>
 			</div>
 		);
 	}
 
-	renderContent() {
-		const { selectedMenu } = this.state;
-
+	function renderContent() {
 		switch (selectedMenu) {
 			case 0:
-				return this.renderApps();
+				return renderApps();
 			case 1:
-				return this.renderSettings();
+				return renderSettings();
 			default:
 				return null;
 		}
 	}
 
-	render() {
-		const { classes } = this.props;
-		const { selectedMenu } = this.state;
-
-		return (
-			<div>
-				<Grid container spacing={2}>
-					<Grid item xs={12} sm={4} md={3} lg={2}>
-						<List className={classes.listMenu}>
-							<ListItem
-								button
-								selected={selectedMenu === 0}
-								onClick={() => this.handleListClick(0)}
-							>
-								<ListItemIcon>
-									<i className={`material-icons ${classes.appIcon}`}>{"apps"}</i>
-								</ListItemIcon>
-								<ListItemText primary="Apps" />
-							</ListItem>
-							<ListItem
-								button
-								selected={selectedMenu === 1}
-								onClick={() => this.handleListClick(1)}
-							>
-								<ListItemIcon>
-									<i className={`material-icons ${classes.appIcon}`}>{"settings"}</i>
-								</ListItemIcon>
-								<ListItemText primary="Settings" />
-							</ListItem>
-						</List>
-					</Grid>
-					<Grid item xs={12} sm={8} md={9} lg={10}>
-						{this.renderContent()}
-					</Grid>
-				</Grid >
-			</div>
-		);
-	}
+	return (
+		<div>
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={4} md={3} lg={2}>
+					<List className={classes.listMenu}>
+						<ListItem
+							button
+							selected={selectedMenu === 0}
+							onClick={() => handleListClick(0)}
+						>
+							<ListItemIcon>
+								<i className={`material-icons ${classes.appIcon}`}>{"apps"}</i>
+							</ListItemIcon>
+							<ListItemText primary="Apps" />
+						</ListItem>
+						<ListItem
+							button
+							selected={selectedMenu === 1}
+							onClick={() => handleListClick(1)}
+						>
+							<ListItemIcon>
+								<i className={`material-icons ${classes.appIcon}`}>{"settings"}</i>
+							</ListItemIcon>
+							<ListItemText primary="Settings" />
+						</ListItem>
+					</List>
+				</Grid>
+				<Grid item xs={12} sm={8} md={9} lg={10}>
+					{renderContent()}
+				</Grid>
+			</Grid >
+		</div>
+	);
 }
-
-Settings.contextType = UserContext;
 
 Settings.propTypes = {
 	classes: PropTypes.object.isRequired,
