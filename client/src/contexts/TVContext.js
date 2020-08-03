766bbec515820a2c960f62a1783413c2162bfc68
dedeco99 <dedeco99@gmail.com>
2020-08-03 19:32:27 +0100

    WIP: Added TVContext files

---
+++ client/src/contexts/TVContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useReducer } from "react";
+import PropTypes from "prop-types";
+
+import { tvReducer } from "../reducers/TVReducer";
+
+export const TVContext = createContext();
+
+const TVContextProvider = ({ children }) => {
+	const initState = {
+		follows: [],
+		subscriptions: [],
+		feeds: [],
+	};
+
+	const [state, dispatch] = useReducer(tvReducer, initState);
+
+	return <TVContext.Provider value={{ state, dispatch }}>{children}</TVContext.Provider>;
+};
+
+TVContextProvider.propTypes = {
+	children: PropTypes.object.isRequired,
+};
+
+export default TVContextProvider;
---
+++ client/src/reducers/TVReducer.js
@@ -0,0 +1,54 @@
+export const tvReducer = (state, action) => {
+	let { follows, subscriptions, feeds } = state;
+
+	switch (action.type) {
+		case "SET_FOLLOWS":
+			follows = action.follows.filter(f => !subscriptions.map(s => s.externalId).includes(f.externalId));
+
+			return { ...state, follows };
+		case "ADD_FOLLOW":
+			follows.push(action.follow);
+
+			return { ...state, follows };
+		case "DELETE_FOLLOW":
+			follows = follows.filter(f => f._id !== action.follow._id);
+
+			return { ...state, follows };
+		case "SET_SUBSCRIPTIONS":
+			follows = follows.filter(f => subscriptions.map(s => s.externalId).includes(f.externalId));
+
+			return { ...state, follows, subscriptions: action.subscriptions };
+		case "ADD_SUBSCRIPTION":
+			subscriptions = [...subscriptions, ...action.subscription].sort((a, b) =>
+				a.displayName.toLowerCase() <= b.displayName.toLowerCase() ? -1 : 1,
+			);
+
+			follows = follows.filter(f => !subscriptions.map(s => s.externalId).includes(f.externalId));
+
+			return { ...state, follows, subscriptions };
+		case "DELETE_SUBSCRIPTION":
+			subscriptions = subscriptions.filter(s => s._id !== action.subscription._id);
+
+			follows = [...follows, action.subscription].sort((a, b) =>
+				a.displayName.toLowerCase() <= b.displayName.toLowerCase() ? -1 : 1,
+			);
+
+			return { ...state, follows, subscriptions };
+		case "SET_FEEDS":
+			return { ...state, feeds: action.feeds };
+		case "ADD_FEED":
+			feeds.push(action.feed);
+
+			return { ...state, feeds };
+		case "EDIT_FEED":
+			feeds = [...feeds.filter(c => c._id !== action.feed._id), action.feed];
+
+			return { ...state, feeds };
+		case "DELETE_FEED":
+			feeds = feeds.filter(c => c._id !== action.feed._id);
+
+			return { ...state, feeds };
+		default:
+			return state;
+	}
+};
