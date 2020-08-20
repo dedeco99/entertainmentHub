ed04b94df90061ac90cb2ded8e1adf92655ec36f
dedeco99 <dedeco99@gmail.com>
2020-08-20 17:50:03 +0100

    Moved notification format to utils

--- client/src/components/.partials/SocketClient.js
+++ client/src/components/.partials/SocketClient.js
@@ -4,6 +4,8 @@ import socketio from "socket.io-client";
 import { UserContext } from "../../contexts/UserContext";
 import { NotificationContext } from "../../contexts/NotificationContext";
 
+import { formatNotification } from "../../utils/utils";
+
 function SocketClient() {
 	const { user } = useContext(UserContext);
 	const { dispatch } = useContext(NotificationContext);
@@ -17,27 +19,14 @@ function SocketClient() {
 
 		socket.on("notification", notification => {
 			dispatch({ type: "ADD_NOTIFICATION", notification });
-			
-			if(Notification.permission === "granted"){
-				renderBrowserNotifications(notification);
+
+			if (Notification.permission === "granted") {
+				const { thumbnail, title, subtitle } = formatNotification(notification);
+
+				return new Notification(title, { body: subtitle, icon: thumbnail });
 			}
 		});
 	}, []);
-
-	function renderBrowserNotifications(showNotification){
-		switch (showNotification.type) {
-			case "youtube":
-				return new Notification(showNotification.info.displayName, { 
-					body: showNotification.info.videoTitle, 
-					icon: showNotification.info.thumbnail 
-				});
-			case "tv":
-				return new Notification(showNotification.info.displayName, { 
-					body: "Season " + showNotification.info.season + " Episode " + showNotification.info.number,
-					icon: showNotification.info.thumbnail 
-				});
-		}
-	}
 
 	return null;
 }
