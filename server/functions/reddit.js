ade6eaa4c14a7cacff9c9501d17dc50bb2f478d4
dedeco99 <dedeco99@gmail.com>
2021-02-26 22:00:50 +0000

    Fixed empty flairs

--- server/functions/reddit.js
+++ server/functions/reddit.js
@@ -63,7 +63,7 @@ function formatResponse(json) {
 			score: data.score,
 			comments: data.num_comments,
 			crossposts: data.num_crossposts,
-			flairs: data.link_flair_richtext.map(flair => flair.t).filter(flair => flair),
+			flairs: data.link_flair_richtext.map(flair => flair.t).filter(flair => flair && flair !== " "),
 			author: data.author,
 			stickied: data.stickied,
 			domain: data.domain,
